7d29ef40bc90bb974fa12ba1b660c8d892283a30
paolocorti <paolocorti87@gmail.com>
2022-03-30 10:15:13 +0200

    update

--- config.js
+++ config.js
@@ -4,4 +4,4 @@ export const server = dev
   ? "http://localhost:3000"
   : "https://creds.vercel.app/";
 
-export const siteUrl = "https://creds.vercel.app/";
+export const siteUrl = "https://creds.vercel.app";
