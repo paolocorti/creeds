93a7a84a075c116cfb0e38792689418230a839c7
Paolo <paolocorti87@gmail.com>
2022-04-13 19:40:15 +0200

    fix layout

--- components/RightColumn.jsx
+++ components/RightColumn.jsx
@@ -11,7 +11,7 @@ const RightColumn = ({ children, expanded = true, fullscreen }) => {
 
   return (
     <div
-      className={`w-full px-4 md:px-8 py-8 bg-pink border-b border-black overflow-hidden relative md:min-h-screen w-full md:w-28`}
+      className={`w-full px-4 md:px-8 py-8 bg-pink border-b border-black overflow-hidden relative md:min-h-screen w-full md:w-72`}
     >
       {children}
     </div>
