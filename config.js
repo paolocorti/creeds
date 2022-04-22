a7420364e6ba82f40bde7971f9de0edf17849383
Paolo <paolocorti87@gmail.com>
2022-04-22 21:52:49 +0200

    fix server url

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://creds.vercel.app/";
+  : "https://creds.vercel.app";
 
 export const siteUrl = "https://creds.vercel.app";
